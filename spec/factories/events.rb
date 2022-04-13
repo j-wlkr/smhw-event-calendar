5059cfe8f9fdbb758b240dcc0b3de9860596cc63
Joshua Walker <josh.wm.walker@gmail.com>
2022-04-13 11:06:08 +0100

    Event model generated and migrated

---
+++ spec/factories/events.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :event do
+    start_date { "2022-04-13 11:04:32" }
+    end_date { "2022-04-13 11:04:32" }
+    title { "MyString" }
+    description { "MyText" }
+    teacher { "MyString" }
+  end
+end
